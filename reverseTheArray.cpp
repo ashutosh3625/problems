cf4fdc04a97c43efeebea7d56834483a43c39bb5
Ashutosh Kumar <theashutosh3625@gmail.com>
2024-12-21 21:14:13 +0530

    Update reverseTheArray.cpp

--- reverseTheArray.cpp
+++ reverseTheArray.cpp
@@ -1,24 +1,24 @@
 #include<iostream>
 using namespace std;
 int main(){
-int n;
-cout<<"enter the size of array:";
-cin>>n;
-int oldArr[n];
-cout<<"enter the elements of array:";
-for(int i=0;i<n;i++){
-cin>>oldArr[i];
-}
-int NewArr[n];
-for(int i=0;i<n;i++){
-NewArr[i]=oldArr[n-i-1];
-}
-cout<<"reverse array is:";
-for(int i=0;i<n;i++){
-cout<<NewArr[i];
-cout<<" ";
-}
-return 0;
+ int n;
+ cout<<"enter the size of array:";
+ cin>>n;
+ int oldArr[n];
+ cout<<"enter the elements of array:";
+ for(int i=0;i<n;i++){
+     cin>>oldArr[i];
+ }
+ int NewArr[n];
+ for(int i=0;i<n;i++){
+    NewArr[i]=oldArr[n-i-1];
+ }
+ cout<<"reverse array is:";
+ for(int i=0;i<n;i++){
+     cout<<NewArr[i];
+      cout<<" ";
+ }
+ return 0;
 
 
 }
